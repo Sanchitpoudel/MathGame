954c3da53285e7657f914a29eefd707402031d63
Sanchit Poudel <snchtpdl12@gmail.com>
2023-01-11 19:09:43 +0545

    Commit

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -34,11 +34,76 @@ class _HomePageState extends State<HomePage> {
     'X',
     '='
   ];
+  //number A, number B
+  int num1 = 1;
+  int num2 = 1;
 
   void buttonTapped(String button) {
     setState(() {
-      userAnswer += button;
+      if (button == '=') {
+        //check if user is correct or not
+        checkResult();
+      } else if (button == 'C') {
+        userAnswer = '';
+      } else if (button == 'DEL') {
+        if (userAnswer.isNotEmpty) {
+          userAnswer = userAnswer.substring(0, userAnswer.length - 1);
+        }
+      } else if (userAnswer.length < 3) {
+        userAnswer += button;
+      }
     });
+  }
+
+  void checkResult() {
+    if (num1 + num2 == int.parse(userAnswer)) {
+      showDialog(
+          context: context,
+          builder: ((context) {
+            return AlertDialog(
+              backgroundColor: Colors.deepPurple,
+              content: Container(
+                height: 200,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      'Correct!',
+                      style: whiteTextStyle,
+                    ),
+                    GestureDetector(
+                      onTap: goToNextQuestion,
+                      child: Container(
+                        margin: EdgeInsets.all(18),
+                        decoration: BoxDecoration(
+                            color: Colors.deepPurple[300],
+                            borderRadius: BorderRadius.circular(2)),
+                        child: Icon(
+                          Icons.arrow_forward,
+                          color: Colors.white,
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            );
+          }));
+    } else {
+      print('Incorrect');
+    }
+  }
+  //create random numbers
+
+  void goToNextQuestion() {
+    Navigator.of(context).pop();
+    //reset values
+    setState(() {
+      userAnswer = '';
+    });
+
+    //create a new question
+
   }
 
   @override
@@ -59,7 +124,8 @@ class _HomePageState extends State<HomePage> {
                   child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text('1 + 1 =  ', style: whiteTextStyle),
+                  Text(num1.toString() + " + " + num2.toString() + " = ",
+                      style: whiteTextStyle),
                   Container(
                     height: 60,
                     width: 140,
